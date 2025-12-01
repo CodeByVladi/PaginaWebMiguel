5c507be9a4f9a9cf7ef45986d6a3c440af6a3cf3
CodeByVladi <vladimiragb01@gmail.com>
2025-12-01 23:18:21 +0100

    feat(videos): rename files, remove videos 2 and 6, add poem titles

--- src/mock/videos.js
+++ src/mock/videos.js
@@ -4,73 +4,57 @@ export const videos = [
   {
     id: 'video-1',
     type: 'local',
-    title: 'Poema',
+    title: 'Espina de rosa',
     reciter: 'Cristina Mojica',
     description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122831.mp4'
+    url: '/videos/Cristina_Mojica.mp4'
   },
   {
     id: 'video-2',
     type: 'local',
-    title: 'Poema',
-    reciter: 'Cristina Mojica',
+    title: 'Amame',
+    reciter: 'Aracely Estrada',
     description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122832.mp4'
+    url: '/videos/Aracely_Estrada.mp4'
   },
   {
     id: 'video-3',
     type: 'local',
-    title: 'Poema',
-    reciter: 'Aracely Estrada',
+    title: 'Manos de luz y tierra',
+    reciter: 'Patricia Hernandez',
     description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122833.mp4'
+    url: '/videos/Patricia_Hernandez.mp4'
   },
   {
     id: 'video-4',
     type: 'local',
-    title: 'Poema',
-    reciter: 'Patricia Hernandez',
+    title: 'Flor de humo',
+    reciter: 'Patricia Valverde',
     description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122834.mp4'
+    url: '/videos/Patricia_Valverde.mp4'
   },
   {
     id: 'video-5',
     type: 'local',
-    title: 'Poema',
-    reciter: 'Patricia Valverde',
+    title: 'Guadalupe caudilla del maíz y del hambre',
+    reciter: 'Pedro Ambrogi',
     description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122835.mp4'
+    url: '/videos/Pedro_Ambrogi.mp4'
   },
   {
     id: 'video-6',
     type: 'local',
-    title: 'Poema',
-    reciter: 'Patricia Valverde',
+    title: 'Amor en ruinas',
+    reciter: 'Angie Navarrete',
     description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122836.mp4'
+    url: '/videos/Angie_Navarrete.mp4'
   },
   {
     id: 'video-7',
     type: 'local',
-    title: 'Poema',
-    reciter: 'Pedro Ambrogi',
-    description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122837.mp4'
-  },
-  {
-    id: 'video-8',
-    type: 'local',
-    title: 'Poema',
-    reciter: 'Angie Navarrete',
-    description: 'Video enviado por colaborador.',
-    url: '/videos/video5868484180421122852.mp4'
-  },
-  {
-    id: 'video-9',
-    type: 'local',
-    title: 'Poema',
-    reciter: 'Miguel Gonzalez',
+    title: 'El autor presenta sus libros',
+    reciter: 'Miguel Ángel González',
     description: 'Autor de todos los poemas.',
-    url: '/videos/video5868484180421122853.mp4'
+    url: '/videos/Miguel_Gonzalez.mp4'
   }
 ];
